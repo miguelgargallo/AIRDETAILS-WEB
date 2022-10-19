3b4a0f2840e0b31afcf605f1a808cbab4941f7bf
Miguel Gargallo <5947268+miguelgargallo@users.noreply.github.com>
2022-10-19 17:07:35 +0200

    1.0.4 GA

---
+++ pages/GA.js
@@ -0,0 +1,24 @@
+import Script from 'next/script'
+
+function Home() {
+  return (
+    <div className="container">
+      <!-- Global site tag (gtag.js) - Google Analytics -->
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-DEFER9ZCKT"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-DEFER9ZCKT');
+        `}
+      </Script>
+    </div>
+  )
+}
+
+export default Home
