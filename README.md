4d8060ede3483c3e3a4b33e6ee67e2b3bb1cc688
miguelgargallo <5947268+miguelgargallo@users.noreply.github.com>
2022-11-09 18:31:43 +0100

    13.0.0 Next13 Update, from the rails to superlight

--- README.md
+++ README.md
@@ -1,34 +1,66 @@
-This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
+#### This is a Next 13 project app.
 
-## Getting Started
+![ITAMAESAN SushiJS Wallpaper Best Light NNextJS Framework with Husky fixed](https://user-images.githubusercontent.com/5947268/185643867-81854585-1f24-470e-9836-2a8b9c62c8dd.png)
 
-First, run the development server:
 
-```bash
-npm run dev
-# or
-yarn dev
-```
+## ITAMAESAN
+### ITAMAESAN ORG is an open source organization that aims to promote the use of open source software in the web development industry.
 
-Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
+- [ITAMAESAN](https://github.com/itamaesanorg/)
+- [SushiJS](https://github.com/sushi-js/)
 
-You can start editing the page by modifying `pages/index.js`. The page auto-updates as you edit the file.
 
-[API routes](https://nextjs.org/docs/api-routes/introduction) can be accessed on [http://localhost:3000/api/hello](http://localhost:3000/api/hello). This endpoint can be edited in `pages/api/hello.js`.
+## Learn with ITAMAESAN University
 
-The `pages/api` directory is mapped to `/api/*`. Files in this directory are treated as [API routes](https://nextjs.org/docs/api-routes/introduction) instead of React pages.
+### Learn how SushiJS is built via ITAMAESAN LEARN on: How To NextJS At ITAMAESAN we are onboarding new education libraries for thoese who wants to initiate on NextJS, the core of SushiJS.
+### ITAMAESAN makes a huge effort to deal and learn on how to fix their goals everyday at the office.
+### This is why we decied to document every error and mention the author!
 
-## Learn More
+- [How To NextJS](https://github.com/itamaesanorg/How-To-NextJS)
+- [edu](https://github.com/itamaesanorg/edu)
+- [Python Basics](https://github.com/itamaesanorg/python-basics)
 
-To learn more about Next.js, take a look at the following resources:
+- [Lab Excercise, News NextJS App](https://github.com/itamaesanorg/news-app)
 
-- [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
-- [Learn Next.js](https://nextjs.org/learn) - an interactive Next.js tutorial.
 
-You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js/) - your feedback and contributions are welcome!
+## About SushiJS
 
-## Deploy on Vercel
+### ITAMAESAN developed SushiJS Framework, SushiJS, a complete utlimate NextJS 12, TypeScript, TailwindCSS with Framer Motion.
 
-The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
+- [The Framework, SushiJS](https://github.com/itamaesanorg/SushiJS)
+- [The Boilerplate, SushiJS](https://github.com/sushi-js/SushiJS-Example-01)
+- [Real Business Case, SushiJS](https://demo-01.sushijs.com/)
 
-Check out our [Next.js deployment documentation](https://nextjs.org/docs/deployment) for more details.
+
+## About Wri
+
+### Enjoy the moment with momento, the best way to admire and show your components or projects like never before to clients, a standard diamond in the future of the industry, it is a showcase component or project site generator, install & play.
+
+- [Wri](https://github.com/itamaesanorg/Wri)
+- [Wri on npm](https://www.npmjs.com/package/wri)
+
+
+## About Helpdesk
+
+### You will be able to publish your docs from online services with a one click install integration, called, helpdesk, sooner we will integrate python and rust too! Stay tuned!
+
+- [helpdesk](https://github.com/itamaesanorg/helpdesk)
+- [helpdesk on npm](https://www.npmjs.com/package/helpdesk)
+
+
+## About Giveaway Tool
+
+### ITAMAESAN also works for private companies, with one exclusive clause, work always with MIT Licenses and release the code on day 1.
+### Giveaway Tool is a website HTML to create giveways. Just upload and play.
+
+- [Giveaway code](https://github.com/itamaesanorg/giveawaytool)
+- [Giveaway in Action](https://giveawaytool.sushijs.com/)
+
+
+## Official Sites
+- [ITAMAESAN Site](https://itamaesan.org)
+- [SushiJS Site](https://sushijs.com)
+
+
+## Twitter
+- [Twitter](https://twitter.com/itamaesanorg)
---
+++ app/page.tsx
@@ -0,0 +1,144 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+          Welcome to{" "}
+          <a
+            href="https://instagram.com/miguelgargallo.es"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            AirDetails!
+          </a>
+        </h1>
+
+        <p className={styles.description}>
+          Get started by knowing our{" "}
+          <code className={styles.code}>VIP events</code>
+        </p>
+
+        <div className={styles.grid}>
+          <a
+            href="https://express.adobe.com/page/sTLdQH7sQVsgb/index.html"
+            className={styles.card}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <h2>VIP Galas &rarr;</h2>
+            <p>
+              Premios ESLAND, IBAI, El Xokas, IlloJuan, Jordi Wild, Jagger ...
+              The Grefg!
+            </p>
+          </a>
+
+          <a
+            href="https://express.adobe.com/page/OopViOPVJhVgV/index.html"
+            className={styles.card}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <h2>The FIELD &rarr;</h2>
+            <p>
+              FC Barcelona, Barça, Barça B, Femeni, Basquet, Events Burocratics
+              i Hockey.
+            </p>
+          </a>
+
+          <a
+            href="https://express.adobe.com/page/t6UQMW9VDePJP/index.html"
+            target="_blank"
+            rel="noopener noreferrer"
+            className={styles.card}
+          >
+            <h2>Surfing skies &rarr;</h2>
+            <p>Mavic 2 Pro built in Hasselblad camera shootings sessions.</p>
+          </a>
+
+          <a
+            href="https://express.adobe.com/page/podDpYbThdPUR/index.html"
+            className={styles.card}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <h2>Thanks ELON Musk &rarr;</h2>
+            <p>Tesla Motors collab, Model X, Barcelona EXPO.</p>
+          </a>
+
+          <a
+            href="https://mailto:miguelgargalloes@gmail.com"
+            className={styles.card}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <h2>Send me Mail! &rarr;</h2>
+            <p>Contact me for any kind of event or project.</p>
+          </a>
+
+          <a
+            href="https://twitter.com/miguelgargallo"
+            className={styles.card}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <h2>DM on Twitter now &rarr;</h2>
+            <p>Send me a DM on Twitter for any kind of event or dev project.</p>
+          </a>
+        </div>
+
+        <p className={styles.description}>
+          I am a ,{" "}
+          <code className={styles.code}>Javascript and Typescript</code>and
+          <code className={styles.code}>React and Next developer</code>.
+        </p>
+
+        <div className={styles.grid}>
+          <a
+            href="https://twitter.com/superdatas"
+            className={styles.card}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <h2>Superdatas projects &rarr;</h2>
+            <p>Pylar draws what you want with Stable Diffusion.</p>
+          </a>
+
+          <a
+            href="https://huggingface.co/miguelgargallo/huggingtweets?text=My+dream+is"
+            className={styles.card}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <h2>Artificial Intelligence &rarr;</h2>
+            <p>
+              I write a free AI license called Pylar AI creative ML license.
+            </p>
+          </a>
+
+          <a
+            href="https://github.com/miguelgargallo"
+            className={styles.card}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <h2>See my Dev profile &rarr;</h2>
+            <p>See my Github profile for more info.</p>
+          </a>
+        </div>
+      </main>
+
+      <footer className={styles.footer}>
+        <a
+          href="https://github.com/miguelgargallo/AIRDETAILS-WEB"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          See the code. 2022 © AirDetails, all rights reserved.
+        </a>
+      </footer>
+    </div>
+  );
+}
